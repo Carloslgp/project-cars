7a08b1cb221577e2a37326983556f93b7f626384
Carlos Leonardo <carloslgp3585@gmail.com>
2026-05-27 19:44:02 -0300

    feat: 3d object appearing on the SimulationScreen.py

--- src/ui/SimulationScreen.py
+++ src/ui/SimulationScreen.py
@@ -1,6 +1,9 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QVBoxLayout, QStackedWidget, QLabel
 from PySide6.QtCore import Signal
 
+from pyvistaqt import QtInteractor
+import pyvista as pv
+
 
 class SimulationScreen(QWidget):
     back_clicked = Signal()
@@ -8,15 +11,18 @@ class SimulationScreen(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.simulation_layout = QHBoxLayout()
+        self.simulation_layout = QVBoxLayout()
 
-        self.simulation_label = QLabel("Simulation Results")
-        self.simulation_layout.addWidget(self.simulation_label)
+        self.plotter = QtInteractor(self)
+        self.simulation_layout.addWidget(self.plotter)
 
         self.back_button = QPushButton("Back")
         self.simulation_layout.addWidget(self.back_button)
 
         self.back_button.clicked.connect(self.change_to_form)
+
+        cubo = pv.Cube()
+        self.plotter.add_mesh(cubo, color="red")
 
         self.setLayout(self.simulation_layout)
 
